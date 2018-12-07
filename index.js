94dffc5516ce9aaaf5c4b0f1fce56087824a0c3d
yuta0801 <yuta20030801@icloud.com>
2018-12-07 17:50:59 +0900

    Remove unnecessary trimming

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inquirer = require('inquirer')
 const prefs = new Preferences('discord.js-repl')
 
 !(async () => {
-  if (process.argv[2]) prefs.token = trimming(process.argv[2])
+  if (process.argv[2]) prefs.token = process.argv[2]
   if (!prefs.token) prefs.token = await askToken()
   process.env.TOKEN = prefs.token
   require('./discord')
@@ -18,11 +18,7 @@ async function askToken() {
     message: 'Enter your Discord token',
     validate: val => val ? true : 'Please enter your Discord token',
   })
-  return trimming(token)
-}
-
-function trimming(str) {
-  return str.trim().match(/^"?([^"]+)"?$/)[1]
+  return token
 }
 
 process.on('unhandledRejection', err => console.log(err))
