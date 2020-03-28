3aa035b92de24e25efb945a8d835b74dcbbf0198
yuta0801 <yuta20030801@icloud.com>
2020-03-29 05:51:32 +0900

    Add onceMsg function to listen message once

--- discord.js
+++ discord.js
@@ -6,23 +6,31 @@ module.exports = (version, token, prefs) => {
 
   console.log(`Node.js ${process.version}, Discord.js ${Discord.version}`)
 
-  const refs = { onMessage: () => {} }
+  const refs = { onceMessage: null, onMessage: null }
 
   client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
     prefs[client.user.tag] = token
     const r = repl.start()
     r.context.client = client
+    r.context.onceMsg = onceMsg
     r.context.onMsg = onMsg
     r.context.offMsg = offMsg
     r.context.Discord = Discord
     r.on('exit', () => process.exit())
   })
 
-  client.on('message', message => refs.onMessage(message))
+  client.on('message', message => {
+    if (refs.onMessage) refs.onMessage(message)
+    if (refs.onceMessage) {
+      refs.onceMessage(message)
+      refs.onceMessage = null
+    }
+  })
 
   client.login(token)
 
+  const onceMsg = callback => refs.onceMessage = callback
   const onMsg = callback => refs.onMessage = callback
-  const offMsg = () => refs.onMessage = () => {}
+  const offMsg = () => refs.onMessage = null
 }
