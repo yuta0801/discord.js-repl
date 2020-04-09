c47039860630e56f2defa5becfbbc5cec43df511
yuta0801 <yuta20030801@icloud.com>
2020-04-09 22:33:56 +0900

    Add patch to login method for login without token

--- run.js
+++ run.js
@@ -7,6 +7,13 @@ module.exports = (version, token) => {
 
   const Discord = require('discord.js')
 
+  if (version === 'v11') {
+    const original = Discord.Client.prototype.login
+    Discord.Client.prototype.login = function (_) {
+      return original.call(this, _ || token)
+    }
+  }
+
   console.log(`Node.js ${process.version}, Discord.js ${Discord.version}`)
 
   const file = process.argv.slice(2).find(arg =>
