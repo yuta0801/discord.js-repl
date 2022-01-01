a2a4350d53894096c4cfa5f1e08bc858dcf21c93
yuto0214w <yuto0214w@gmail.com>
2022-01-02 00:42:46 +0900

    Fix message event deprecation and delete login patch for v11

--- discord.js
+++ discord.js
@@ -3,7 +3,7 @@ const repl = require('repl')
 module.exports = (version, token, prefs) => {
   const Discord = require('discord.js-' + version)
   const client = new Discord.Client({
-    intents: Object.keys(Discord.Intents.FLAGS)
+    intents: Object.keys(Discord.Intents.FLAGS),
   })
 
   console.log(`Node.js ${process.version}, Discord.js ${Discord.version}`)
@@ -22,7 +22,7 @@ module.exports = (version, token, prefs) => {
     r.on('exit', () => process.exit())
   })
 
-  client.on('message', message => {
+  client.on(version === 'v12' ? 'message' : 'messageCreate', message => {
     if (refs.onMessage) refs.onMessage(message)
     if (refs.onceMessage) {
       refs.onceMessage(message)
